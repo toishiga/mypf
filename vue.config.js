68161e278e568a4d1a33ec0765d35b85365d760c
toishiga <toishiga@gmail.com>
2022-09-15 15:33:39 +0900

    pubpath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'mypf',
+  publicPath: '/mypf',
   transpileDependencies: true,
   outputDir: 'docs'
 })
